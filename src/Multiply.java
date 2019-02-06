d8807fb97ecbca964388442092ccb81a2d1ab206
Ankita Koratkar <ankitakoratkar@Sunitas-MacBook-Pro.local>
2019-02-06 15:15:39 -0800

    Initial Commit

---
+++ src/Multiply.java
@@ -0,0 +1,10 @@
+
+public class Multiply {
+	public static void main (String args[]){
+		Multiply mult = new Multiply();
+		System.out.println(mult.multiply(9, 10));
+	}
+	public int multiply(int a, int b){
+		return a*b;
+	}
+}
