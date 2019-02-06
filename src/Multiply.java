6af90d450b955e66406d2c1ff4fefd2c77d2b2e9
Ankita Koratkar <ankitakoratkar@Sunitas-MacBook-Pro.local>
2019-02-06 15:17:28 -0800

    Changed numbers

--- src/Multiply.java
+++ src/Multiply.java
@@ -2,7 +2,7 @@
 public class Multiply {
 	public static void main (String args[]){
 		Multiply mult = new Multiply();
-		System.out.println(mult.multiply(9, 10));
+		System.out.println(mult.multiply(8, 10));
 	}
 	public int multiply(int a, int b){
 		return a*b;
